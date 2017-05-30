e2e818fa347c56cd0756bc5a785f950383b9965e
kunal-pmj <singhk@vmware.com>
2017-05-30 10:52:11 +0530

    Addressee a minor issue of Correcting the message. The change needs to be integrated as these tests are based on comparion of these messages Signed-off-by: Kunal Singh <singhk@vmware.com>

--- src/main/java/vmware/samples/contentlibrary/isomount/IsoMount.java
+++ src/main/java/vmware/samples/contentlibrary/isomount/IsoMount.java
@@ -110,7 +110,7 @@ public class IsoMount extends SamplesAbstractBase {
         itemFindSpec.setName(itemName);
         itemFindSpec.setType(ISO_TYPE);
         List<String> isoItemIds = client.itemService().find(itemFindSpec);
-        assert isoItemIds.size() == 1 : "library item on name " + itemName
+        assert isoItemIds.size() == 1 : "library item by name " + itemName
                 + " and type " + ISO_TYPE + " must exist";
         return isoItemIds.get(0);
     }
