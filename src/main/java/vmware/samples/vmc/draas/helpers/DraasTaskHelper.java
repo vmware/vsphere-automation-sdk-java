4589b9083e222c03bc45aa3b6c479d15cb57fdc5
JOBIN GEORGE <jgeorge@vmware.com>
2019-09-10 21:28:05 +0530

    Added copyright to the DraaS Samples
    Signed-off-by: JOBIN GEORGE <jgeorge@vmware.com>

--- src/main/java/vmware/samples/vmc/draas/helpers/DraasTaskHelper.java
+++ src/main/java/vmware/samples/vmc/draas/helpers/DraasTaskHelper.java
@@ -1,3 +1,15 @@
+/*
+ * *******************************************************
+ * Copyright VMware, Inc. 2019.  All Rights Reserved.
+ * SPDX-License-Identifier: MIT
+ * *******************************************************
+ *
+ * DISCLAIMER. THIS PROGRAM IS PROVIDED TO YOU "AS IS" WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, WHETHER ORAL OR WRITTEN,
+ * EXPRESS OR IMPLIED. THE AUTHOR SPECIFICALLY DISCLAIMS ANY IMPLIED
+ * WARRANTIES OR CONDITIONS OF MERCHANTABILITY, SATISFACTORY QUALITY,
+ * NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+ */
 package vmware.samples.vmc.draas.helpers;
 
 import java.util.Date;
