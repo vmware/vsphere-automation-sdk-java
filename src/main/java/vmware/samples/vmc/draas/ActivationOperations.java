c60c85c794aff386195c7b9c56a3926ac7215aad
amanp <amanp@vmware.com>
2019-11-25 14:44:54 +0530

    Updating ActivationOperations.java to include optional parameters Signed-off-by: Aman Priyadarshi <amanp@vmware.com>

--- src/main/java/vmware/samples/vmc/draas/ActivationOperations.java
+++ src/main/java/vmware/samples/vmc/draas/ActivationOperations.java
@@ -89,7 +89,7 @@ public class ActivationOperations extends VmcSamplesAbstractBase {
 	@Override
 	protected void cleanup() throws Exception {
 	    Task siteRecoveryDeActivationTask =
-				siteRecoveryStub.delete(orgId, sddcId, null);
+				siteRecoveryStub.delete(orgId, sddcId, false, false);
 		String taskId =
 				siteRecoveryDeActivationTask.getId();
 		System.out.printf("Site Recovery Add-On Task De-Activation ID  %s", taskId);
