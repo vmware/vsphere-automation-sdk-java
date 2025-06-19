6b191a904383377f4a60aa9ad4435d767202854f
Shweta Purohit <shweta.purohit@broadcom.com>
2025-06-19 15:16:12 +0530

    vSphere Automation SDK Java 9.0 Release
    Signed-off-by: Shweta Purohit <shweta.purohit@broadcom.com>

--- src/main/java/vmware/samples/common/AuthorizationConstants.java
+++
@@ -1,8 +0,0 @@
-package vmware.samples.common;
-
-public class AuthorizationConstants {
-    public static final String CLIENT_ID="clientId";
-    public static final String CLIENT_SECRET="clientSecret";
-    public static final String REDIRECT_URI="redirect_uri";
-    public static final String REDIRECT_URIS="redirectUris";
-}
